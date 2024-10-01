a457f77ff61fe5bf9cbeee1eabf44bb06b4a63cc
Darshan S <hitmandarshan34@gmail.com>
2024-10-01 23:05:44 +0530

    chatuser react query implimented

--- frontend/src/components/ChatUserList.jsx
+++ frontend/src/components/ChatUserList.jsx
@@ -3,12 +3,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { ScrollArea } from "./ui/scroll-area";
 import { setSelectedUser } from "@/redux/chatSlice";
+import { Loader } from "lucide-react";
 
 const ChatUserList = () => {
   const dispatch = useDispatch();
-  const { user, suggestedUsers } = useSelector((store) => store.auth);
+  const { user } = useSelector((store) => store.auth);
   const [selectedUserTab, setSelectedUserTab] = useState();
   const { onlineUsers } = useSelector((store) => store.chat);
+
+  const fetchSuggestedUsers = async () => {
+    const res = await axios.get(
+      "http://localhost:5000/api/v1/user/suggestedusers",
+      {
+        withCredentials: true,
+      }
+    );
+    return res.data.users;
+  };
+
+  const { isPending, data: suggestedUsers } = useQuery({
+    queryKey: ["chatsuggestedUsers"],
+    queryFn: fetchSuggestedUsers,
+    staleTime: 60000,
+  });
 
   const handleUserClick = (selectedUser) => {
     dispatch(setSelectedUser(selectedUser));
@@ -36,43 +53,49 @@ const ChatUserList = () => {
           </p>
         </div>
       </div>
-      <ScrollArea>
-        {suggestedUsers?.map((suggestedUser) => {
-          const isOnline = onlineUsers.includes(suggestedUser?._id);
-          return (
-            <div
-              key={suggestedUser?._id}
-              className={`flex items-center gap-x-4 ${
-                suggestedUser?._id === selectedUserTab
-                  ? "bg-slate-100 rounded-lg"
-                  : ""
-              } my-3 cursor-pointer p-2`}
-              onClick={() => handleUserClick(suggestedUser)}
-            >
-              <Avatar className="w-12 h-12 mx-auto md:mx-0">
-                <AvatarImage src={suggestedUser?.profilePicture} />
-                <AvatarFallback>
-                  {suggestedUser?.userName?.slice(0, 2)?.toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-              <div>
-                <h3 className="text-md font-bold  hidden md:block">
-                  {suggestedUser?.userName}
-                </h3>
-                {isOnline ? (
-                  <p className=" text-green-500  text-sm font-semibold  hidden md:block">
-                    Online
-                  </p>
-                ) : (
-                  <p className=" text-red-600 text-sm font-semibold  hidden md:block">
-                    Offline
-                  </p>
-                )}
+      {isPending ? (
+        <div className=" w-full h-full flex justify-center items-center">
+          <Loader className=" animate-spin size-8" />
+        </div>
+      ) : (
+        <ScrollArea>
+          {suggestedUsers?.map((suggestedUser) => {
+            const isOnline = onlineUsers.includes(suggestedUser?._id);
+            return (
+              <div
+                key={suggestedUser?._id}
+                className={`flex items-center gap-x-4 ${
+                  suggestedUser?._id === selectedUserTab
+                    ? "bg-slate-100 rounded-lg"
+                    : ""
+                } my-3 cursor-pointer p-2`}
+                onClick={() => handleUserClick(suggestedUser)}
+              >
+                <Avatar className="w-12 h-12 mx-auto md:mx-0">
+                  <AvatarImage src={suggestedUser?.profilePicture} />
+                  <AvatarFallback>
+                    {suggestedUser?.userName?.slice(0, 2)?.toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  <h3 className="text-md font-bold  hidden md:block">
+                    {suggestedUser?.userName}
+                  </h3>
+                  {isOnline ? (
+                    <p className=" text-green-500  text-sm font-semibold  hidden md:block">
+                      Online
+                    </p>
+                  ) : (
+                    <p className=" text-red-600 text-sm font-semibold  hidden md:block">
+                      Offline
+                    </p>
+                  )}
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </ScrollArea>
+            );
+          })}
+        </ScrollArea>
+      )}
     </div>
   );
 };
