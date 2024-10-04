ce5e7e9390421802619592c39dd62a048e006e1f
Darshan S <hitmandarshan34@gmail.com>
2024-10-04 09:55:38 +0530

    cookies error fixing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -96,7 +96,7 @@ export const login = async (req, res) => {
     return res
       .cookie("token", token, {
         httpOnly: true,
-        sameSite: "none",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
